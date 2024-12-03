34388878c6bd53cf99f92438b51b1313c8a711ac
John Fewell <fewell@gmail.com>
2024-12-03 17:25:31 -0500

    Update type declarations for shapeFromText

---
+++ types/index.d.ts
@@ -0,0 +1,60 @@
+declare module "@fewell/canvas-confetti-ts" {
+  export interface PathShape {
+    type: "path";
+    path: string;
+    matrix: DOMMatrix;
+  }
+
+  export interface BitmapShape {
+    type: "bitmap";
+    bitmap: ImageBitmap;
+    matrix: DOMMatrix;
+  }
+
+  export type Shape = PathShape | BitmapShape | "square" | "circle" | "star";
+
+  export interface Origin {
+    x?: number;
+    y?: number;
+  }
+
+  export interface Options {
+    angle?: number;
+    colors?: string[];
+    decay?: number;
+    disableForReducedMotion?: boolean;
+    drift?: number;
+    gravity?: number;
+    origin?: Origin;
+    particleCount?: number;
+    scalar?: number;
+    shapes?: Shape[];
+    spread?: number;
+    startVelocity?: number;
+    ticks?: number;
+    zIndex?: number;
+    x?: number;
+    y?: number;
+  }
+
+  export interface GlobalOptions {
+    disableForReducedMotion?: boolean;
+    resize?: boolean;
+    useWorker?: boolean;
+  }
+
+  export type Reset = () => void;
+
+  export interface CreateTypes {
+    (options?: Options): Promise<undefined> | null;
+    reset: Reset;
+  }
+
+  const confetti: CreateTypes;
+  export default confetti;
+
+  export function shapeFromText(
+    text: string,
+    options?: { size?: number }
+  ): Shape;
+}
